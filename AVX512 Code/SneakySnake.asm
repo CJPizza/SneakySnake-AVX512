371d3446968a7ae28fdcc7d850d19f168ff02104
czarinadamienne <129986689+czarinadamienne@users.noreply.github.com>
2025-10-26 22:56:20 +0800

    sorta change some registers + more comments

---
+++ AVX512 Code/SneakySnake.asm
@@ -0,0 +1,289 @@
+%include "io.inc"
+
+section .text
+default rel
+bits 64
+
+section .data
+    processed_counter dq 0
+    read_bytes dq 0
+    global_counter dq 0
+    counter dq 0
+
+section .rodata
+    align 64
+    ones_mask: db 0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF
+                db 0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF
+                db 0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF
+                db 0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF
+    four_mask: times 64 db 0x0F ;do we broadcast this to a zmm?
+    four: db 4
+    
+
+global SneakySnake
+
+;int SneakySnake(int ReadLength, char* ReadSeq, char* RefSeq, int EditThreshold, int IterationNo)
+;parameters and their corresponding registers:
+;int ReadLength = rcx
+;char* ReadSeq = rdx
+;char* RefSeq = r8
+;int EditThreshold = r9
+;int IterationNo = [rbp + 8] 
+;return value =	r/eax
+
+SneakySnake:
+    push rbp
+    mov rbp, rsp
+
+    push rdi
+    push rsi
+    push rdx
+    push r12
+    push r13
+    push r14
+    push r15
+    push rbx
+
+    ;move parameters to local registers
+    mov r11, rcx			; ReadLength
+	mov r12, rdx			; ReadSeq
+	mov r13, r8				; RefSeq
+	mov r14, r9				; EditThreshold
+	mov r15, [rbp + 40h]	; IterationNo
+
+	; r11 = ReadLength
+	; r12 = ReadSeq
+	; r13 = RefSeq
+	; r14 = EditThreshold
+	; r15 = IterationNo
+
+    ; computing byte length of the read
+	; since 8 bits is the smallest can we can work on
+	; we need to divide the read length by 2 since working on 4-bit nucleotides
+	mov rdx, r11		
+	shr rdx, 1				; divides readlength by 2
+	mov qword [read_bytes], rdx
+
+	; pointers and counters
+	xor rdx, rdx				; index for the read
+	xor r9, r9					; offset for bytes
+
+    .mainloop:
+        mov rcx, [read_bytes]
+        cmp r9, rcx ;compare offset with read bytes
+        jae .handle_tail
+
+        ; ADDED rsi for storing the score of the best diagonal
+        mov rsi, 64				; max mismatches possible in a 64 byte register
+        ; ^ this gets updated in the diagonal loops, dw about the placeholder 64
+
+        ; load sequences into registers
+        vmovdqu8 zmm0, [r12 + r9] ;read
+        vmovdqu8 zmm1, [r13 + r9] ;ref
+        
+        ;check if current zmms are all 1111s
+        vpcmpeqb k1, zmm0, [ones_mask]    ;cmp if it matches 1111s
+        vpcmpeqb k2, zmm1, [ones_mask]    ;cmp if it matches 1111s
+        kortestq k1, k1
+        kortestq k2, k2
+        jnz .skip ;if yes .skip
+
+        ;do smthn about the 1111s in the sequences
+        ;-------- MAIN DIAGONAL -------------
+        vpandd zmm2, zmm0, [four_mask] ;read low = base1
+        vpandd zmm3, zmm1, [four_mask] ;ref
+        vpcmpeqb k3, zmm2, zmm3 ;cmp if = -> result in k3
+
+        vpbroadcast zmm31, [four]
+        vpsrlvb zmm0, zmm0, zmm31 ;read high = base2
+        vpsrlvb zmm1, zmm1, zmm31 ;ref
+        vpcmpeqb k4, zmm0, zmm1 ;cmp if = -> result in k4
+
+        knotq k3, k3
+        knotq k4, k4 ;invert -> mismatch = 1
+
+        ;check if all bases matched
+        kandq k5, k3, k4
+        kortestq k5, k5
+        jz .exit
+
+        ;borrow stack
+        sub rsp, 16
+
+        kmovq [rsp], k3
+        kmovq [rsp + 8], k4
+
+        mov rax, [rsp]
+        tzcnt rbx, rax ;get the index
+        shl rbx, 1 ;2i -> index and globalcount
+
+        mov r10, [rsp + 8]
+        tzcnt rax, r10
+        lea rax, [rax*2+1] ;2i+1
+        mov r10, rax
+
+        ;compare index position (whoever smallest/if same always pick k3)
+        cmp rbx, r10
+        je .pick1
+        cmova rbx, r10
+        mov qword[global_count], rbx
+
+        .pick1:
+            mov qword[global_count], rbx
+
+        add rsp, 16
+        xor rax, rax
+; ---------- LEFT and RIGHT DIAGONAL ---------------
+; VERY INCOMPLETE
+	; loop from edit distance 1 to edit distance threshold
+	xor rdi, rdi		; the edit distance counter
+	cmp r14, 0			; check if edit distance threshold is 0
+	je .diagonals_done	; if 0, skip diagonals
+
+.diagonal_loop:
+	inc rdi			; increment edit distance
+
+; ----------- DELETION / RIGHT DIAGONAL ---------------
+	vmovdqu8 zmm1, [r13 + r9]	; move ref seq to zmm1 with offset
+	mov rax, r12 				; move read seq pointer to rax
+	; the only difference between the the left and right diagonal since ReadSeq - e
+	sub rax, rdi				; offset read seq pointer by edit distance
+	vmovdqu8 zmm0, [rax + r9]	; move read seq to zmm0
+
+	; 4-bit base logic
+	vpandd zmm2, zmm0, 0x0F		; read low
+	vpandd zmm3, zmm1, 0x0F		; ref low
+	vpcmpeqb k1, zmm2, zmm3		; k1 = low bits comparison
+
+	vpsrlb zmm0, zmm0, 4
+	vpsrlb zmm1, zmm1, 4
+
+	vpandd zmm4, zmm0, 0x0F		; read high
+	vpandd zmm5, zmm1, 0x0F		; ref high
+	vpcmpeqb k2, zmm4, zmm5		; k2 = high bits comparison
+
+	; count mismatches for this diagonal
+	kandw k3, k1, k2			; k3 = combined comparison
+	knotw k3, k3				; invert k3 to get mismatches
+	kpopcntq rax, k3			; count mismatches in k3
+	cmp rax, rsi				; compare with current mismatch count
+	cmovl rsi, rax				; update best mismatch count
+	; ^^^^ this may be wrong, but it finds the best alignment for this read sequence by keeping the lowest mismatch count
+	; so rsi starts at 64, and after checking each diagonal, we will get the mismatches in rax
+	; this will only update rsi only if the current diagonal has less mismatches than the previous best
+	; so after checking all diagonals, rsi will have the lowest mismatch count found
+
+; ----------- INSERTION / LEFT DIAGONAL ---------------
+	vmovdqu8 zmm1, [r13 + r9]	; move ref seq to zmm1 with offset
+	mov rax, r12 				; move read seq pointer to rax
+	; the only difference between the the left and right diagonal since ReadSeq + e
+	add rax, rdi				; offset read seq pointer by edit distance
+	vmovdqu8 zmm0, [rax + r9]	; move read seq to zmm0
+
+	; 4-bit base logic
+	vpandd zmm2, zmm0, 0x0F		; read low
+	vpandd zmm3, zmm1, 0x0F		; ref low
+	vpcmpeqb k1, zmm2, zmm3		; k1 = low bits comparison
+
+	vpsrlb zmm0, zmm0, 4
+	vpsrlb zmm1, zmm1, 4
+
+	vpandd zmm4, zmm0, 0x0F		; read high
+	vpandd zmm5, zmm1, 0x0F		; ref high
+	vpcmpeqb k2, zmm4, zmm5		; k2 = high bits comparison
+
+	; count mismatches for this diagonal
+	kandw k3, k1, k2			; k3 = combined comparison
+	knotw k3, k3				; invert k3 to get mismatches
+	kpopcntq rax, k3			; count mismatches in k3
+	cmp rax, r10				; compare with current mismatch count
+	cmovl rsi, rax
+
+	; check if we reached the edit distance threshold
+	cmp rdi, r14
+	jne .diagonal_loop			; loop again
+
+.diagonals_done:
+	; after checking all diagonals, rsi has the lowest mismatch count found
+	; move the best mismatch count to r10 for final checking
+	mov r10, rsi				; update mismatch count with best found
+
+	;mismatch
+
+	;inc globalcounter
+
+	;wat if we count the total then inc to reduce the branching
+
+	;use mask to check if 0 -> exit: update the checkpoint 
+
+	;count 1s in k masks -> inc matches?
+
+	
+
+
+	; mask -> later can be used to know how many accepts before checkpoint etc
+	; count the mask -> for how many accepts for the given
+
+	
+
+
+
+
+
+	; updating pointers and counters
+	inc qword [processed_counter]		; how many sequences has been processed
+	add r9, 64							; move onto the next byte
+	jmp .mainloop
+
+.exit:
+	;quick exit checker
+	;check if counter == number of bytes in zmm reg
+
+
+.handle_tail:
+	; this is for handling the remaining unused bits in the 512 bit registers
+	mov rcx, [read_bytes]
+	sub rcx, r9
+	test rcx, rcx
+	jz .check_mismatches
+
+	; FINISH THIS TAIL HANDLING
+
+.check_mismatches:
+	; checking if mismatches are within the edit distance threshold
+	mov rax, r10		; number of mismatches
+	cmp rax, r14		; compare with edit threshold
+	jle .accepted		; if less or equal, accepted
+	jg .rejected		; if greater, rejected
+
+.accepted:
+	inc qword [accepted]
+	mov eax, 1			; return 1
+	jmp .cleanup
+
+.rejected:
+	inc qword [rejected]
+	xor eax, eax		; return 0
+	jmp .cleanup
+
+.cleanup:
+	; this just for cleanup before returning
+	add rsp, 32			; restore shadow space
+	pop rbx				; restore registers
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop rdx	
+	pop rsi
+	pop rdi
+	move rsp, rbp
+	pop rbp
+	ret
+
+main:
+	; this is mainly just for testing, this can be removed after everything has been verified
+
+
+	xor eax, eax
+	ret
